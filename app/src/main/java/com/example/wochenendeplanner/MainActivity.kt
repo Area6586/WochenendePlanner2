db934522b77c6e7bd27f311d2d1bc3813dc042b3
Gtam <umt-sch-27@mail.de>
2022-04-24 14:17:15 +0200

    added new repos

---
+++ app/src/main/java/com/example/wochenendeplanner/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wochenendeplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //new repos
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
